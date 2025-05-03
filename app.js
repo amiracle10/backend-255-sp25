7b6c767d555cf6dbccfb4263df6bb46cb69de0e6
amiracle10 <amiracle10@ivytech.edu>
2025-05-03 14:30:36 -0500

    app.js delete and auth and db username

--- app.js
+++ app.js
@@ -1,16 +1,103 @@
 // setup.. this i similar to when we use our default tags in html
 const express = require("express");
-const Song = require("./models/song");
+const Song = require("./models/song.js");
 var cors = require('cors')
+// const bodyParser = require('body-parser')
+const jwt = require('jwt-simple')
+const User = require("./models/users.js")
 
 const app = express();
 app.use(cors())
 app.use(express.json())
 
 const router = express.Router()
-//we have to use cors in order to host a frontend and backend on the same device
-// var cors = require('cors')
-// activate or tell this app varibale to be an express server
+const secret = "supersecret"
+
+//creating an new user
+router.post("/user", async(req,res) =>{
+    if(!req.body.username || !req.body.password){
+        res.status(400).json({error: "Missing username or password"});
+        
+    }
+
+    const newUser = await new User({
+        username: req.body.username, 
+        password: req.body.password,
+        status: req.body.status
+    })
+    
+    try{
+        await newUser.save()
+        console.log(newUser)
+        res.sendStatus(201) //created
+    }
+    catch(err){
+        console.log(err)
+    }
+
+})
+
+//authenticate or login
+
+//post request- reason why is because when you login you are creating what we call a new 'session'
+router.post("/auth", async(req,res) =>{
+    if(!req.body.username || !req.body.password){
+       res.status(401).json({error: "Missing username or password"})
+       return
+    }
+    //find the user in the database
+    let user = await User.findOne({username : req.body.username})
+
+        // bad username
+        if(!user){
+            res.status(401).json({error:"Bad username"})
+        }
+        // check password
+        else{
+            if(user.password != req.body.password){
+                res.status(401).json({error: "Bad password"})
+            }
+            // successful login attempt
+            else{
+                // create token encoded with jwt, and send username
+                // also send back authorization
+                // using boolean or number
+
+                username2 = user.username
+                const token = jwt.encode({username: user.username},secret)
+                const auth = 1
+
+                // respond with token
+                res.json({
+                    username2, 
+                    token: token,
+                    auth: auth
+                })
+            }
+        }
+    })
+
+    //check status of user with a valid token, see if it matches the front ebnd token
+router.get("/status", async(req,res) => {
+    if(!req.headers["x-auth"]){
+        return res.status(401).json({error: "missing x-auth"})
+
+    }
+
+    //if x-auth contains the token
+    const token = req.headers["x-auth"]
+    try{
+        const decoded = jwt.decode(token,secret)
+
+        //send abck all username and status fields to the user or front end
+         let users = User.find({}, "username status")
+         res.json(users)
+    }
+    catch(ex){
+        res.status(401).json({error: "invalid jwt"})
+    }
+})
+
 
 
 
@@ -64,6 +151,20 @@ router.post("/songs", async(req,res) => {
 
     }
     catch (err){
+        res.status(400).send(err)
+    }
+ })
+
+ router.delete("/songs/:id", async(req,res) => {
+    //method or function in mongoose/mongo to delete a single instance of a song or object
+    try {
+        const song = await Song.findById(req.params.id)
+        console.log(song)
+         await Song.deleteOne({_id: req.params.id})
+         res.sendStatus(204)
+    }
+
+    catch(err){
         res.status(400).send(err)
     }
  })
--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://amiracle10:sdev255password@songdb.2xkjaqt.mongodb.net/?retryWrites=true&w=majority&appName=SongDB", {useNewUrlParser: true})
+mongoose.connect("mongodb+srv://amiracle10:sdev255password@songdb.2xkjaqt.mongodb.net/?retryWrites=true&w=majority&appName=SongDB", {useNewUrlParser: true});
 
 module.exports = mongoose
